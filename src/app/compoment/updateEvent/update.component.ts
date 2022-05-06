28f55170408fbd48cc536541006be48b2561d454
huy <huyhq2@bap.jp>
2022-05-06 17:54:48 +0700

    huy

--- src/app/compoment/updateEvent/update.component.ts
+++ src/app/compoment/updateEvent/update.component.ts
@@ -10,120 +10,148 @@ import {EventtService} from "../../service/eventt.service";
   styleUrls: ['./update.component.css']
 })
 export class UpdateComponent implements OnInit {
-  // eventt: Eventt = {
-  //   id: '',
-  //   name: '',
-  //   detail:'',
-  //   time:''
-  // };
-  //
-  // eventForm : FormGroup= this.fb.group({
-  //   name : new FormControl('',Validators.required),
-  //   detail : new FormControl('',Validators.required),
-  //   time : new FormControl('',Validators.required),
-  // })
-  //
-  // constructor(
-  //   private eventtService: EventtService,
-  //   private activatedRoute: ActivatedRoute,
-  //   private fb: FormBuilder,
-  // ) {
-  // }
-  //
-  // ngOnInit(): void {
-  //   this.activatedRoute.paramMap.subscribe(parammap => {
-  //     const id = parammap.get('id');
-  //     console.log("hí");
-  //     // @ts-ignore
-  //     this.eventtService.getId(id).subscribe(result =>{
-  //         this.eventt= result;
-  //         console.log(result)
-  //       },
-  //       error => {
-  //         console.log(error)
-  //       })
-  //   },)
-  //   this.eventt = {
-  //     name: '',
-  //     detail:'',
-  //     time:''    }
-  // }
-  //
-  // updateEventProfile(){
-  //   const event = this.eventForm.value;
-  //   console.log(event);
-  //   // @ts-ignore
-  //   this.eventtService.updateEvent(this.eventt.id,event).subscribe(()=>{
-  //     alert("thành công")
-  //   })
-  // }
+  eventt: Eventt = {
+    name: '',
+    detail:'',
+    time:''
+  };
 
-  // delete(){
-  //   // @ts-ignore
-  //   this.eventtService.deleteEvent(this.eventt.id).subscribe(()=>{
-  //     alert("xóa thành công")
-  //   })
-  // }
-  //
-  // create() {
-  //   const event = this.eventForm.value;
-  //   this.eventtService.addEvent(event).subscribe(() => {
-  //     alert("thêm thành công")
-  //   })
-  // }
-
-  eventts: Eventt[] = [
-    {}
-  ];
-
-  eventForm = new FormGroup( {
-    name: new FormControl(''),
-    detail: new FormControl(''),
-    time: new FormControl(''),
+  eventForm : FormGroup= this.fb.group({
+    name : new FormControl('',Validators.required),
+    detail : new FormControl('',Validators.required),
+    time : new FormControl('',Validators.required),
   })
 
-  constructor(private eventtService: EventtService,
-              private activatedRoute: ActivatedRoute,
-              private fb: FormBuilder,
-              ) {
+  constructor(
+    private eventtService: EventtService,
+    private activatedRoute: ActivatedRoute,
+    private fb: FormBuilder,
+  ) {
   }
 
   ngOnInit(): void {
-    this.eventtService.getAllEvent().subscribe(result => {
-        this.eventts = result;
-        console.log(result);
-      }, error => {
-        console.log(error)
-      }
-    )
     this.activatedRoute.paramMap.subscribe(parammap => {
       const id = parammap.get('id');
-      console.log(id);
-      this.eventtService.getId(id).subscribe(result => {
-          this.eventForm = new FormGroup( {
-            name: new FormControl(result.name),
-            detail: new FormControl(result.detail),
-            time: new FormControl(result.time)
-          })
+      console.log("hí");
+      // @ts-ignore
+      this.eventtService.getId(id).subscribe(result =>{
+          this.eventt= result;
+          console.log(result)
         },
         error => {
           console.log(error)
         })
     },)
+    this.eventt = {
+      name: '',
+      detail:'',
+      time:''    }
   }
 
-  updateEventProfile() {
-    const event = {
-      name: this.eventForm.value.name,
-      detail: this.eventForm.value.detail,
-      time: this.eventForm.value.time
-    }
+  updateEventProfile(){
+    const event = this.eventForm.value;
     console.log(event);
     // @ts-ignore
-    this.eventtService.updateEvent(this.eventts.id, event).subscribe(() => {
-      alert("Sửa thành công!")
+    this.eventtService.updateEvent(this.eventt.id,event).subscribe(()=>{
+      alert("thành công")
     })
   }
 
+  delete(){
+    // @ts-ignore
+    this.eventtService.deleteEvent(this.eventt.id).subscribe(()=>{
+      alert("xóa thành công")
+    })
+  }
 
+  create() {
+    const event = this.eventForm.value;
+    this.eventtService.addEvent(event).subscribe(() => {
+      alert("thêm thành công")
+    })
+  }
+
+  // eventts: Eventt[] = [
+  //   {}
+  // ];
+  //
+  // eventForm = new FormGroup( {
+  //   id: new FormControl(''),
+  //   name: new FormControl(''),
+  //   detail: new FormControl(''),
+  //   time: new FormControl(''),
+  // })
+  //
+  // constructor(private eventtService: EventtService,
+  //             private activatedRoute: ActivatedRoute,
+  //             private fb: FormBuilder,
+  //             ) {
+  // }
+  //
+  // ngOnInit(): void {
+  //   this.eventtService.getAllEvent().subscribe(result => {
+  //       this.eventts = result;
+  //       console.log(result);
+  //     }, error => {
+  //       console.log(error)
+  //     }
+  //   )
+  //   this.activatedRoute.paramMap.subscribe(parammap => {
+  //     const id = parammap.get('id');
+  //     console.log(id);
+  //     this.eventtService.getId(id).subscribe(result => {
+  //         this.eventForm = new FormGroup( {
+  //           id: new FormControl(result.id),
+  //           name: new FormControl(result.name),
+  //           detail: new FormControl(result.detail),
+  //           time: new FormControl(result.time)
+  //         })
+  //       },
+  //       error => {
+  //         console.log(error)
+  //       })
+  //   },)
+  // }
+  //
+  // updateEventProfile() {
+  //   const event = {
+  //     id: this.eventForm.value.id,
+  //     name: this.eventForm.value.name,
+  //     detail: this.eventForm.value.detail,
+  //     time: this.eventForm.value.time
+  //   }
+  //   console.log(event);
+  //   // @ts-ignore
+  //   this.eventtService.updateEvent(this.eventts.id, event).subscribe(() => {
+  //     alert("Sửa thành công!")
+  //   })
+  // }
+
+
+
+  // iEvent = localStorage.getItem("EVENTID")
+  //
+  // // @ts-ignore
+  // eventt?: Eventt
+  //
+  //
+  // constructor(private activatedRoute: ActivatedRoute,
+  //             private eventtService: EventtService) {
+  // }
+  //
+  // ngOnInit(): void {
+  //
+  //   // @ts-ignore
+  //   this.eventtService.getId(this.iEvent).subscribe(result => {
+  //     this.eventt = result
+  //     console.log(result)
+  //   })
+  // }
+  //
+  // updateEvent() {
+  //   // @ts-ignore
+  //   this.eventtService.updateEvent(this.iEvent).subscribe(() => {
+  //     alert("Cập nhật thành công!")
+  //   })
+  // }
 }
